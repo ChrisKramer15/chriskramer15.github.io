82968e6addf84cddf02de928a12db206752c0a24
Chris Kramer <ChrisKramer15@outlook.com>
2020-12-11 16:45:20 -0600

    javascript file created.  navbar creation using js targetting in progress

---
+++ script.js
@@ -0,0 +1,8 @@
+// let navBar = document.querySelector("#beginBtn");
+// let aboutSection = document.querySelector("#about");
+
+// navBar.addEventListener("click", () => {
+//     let newNav = document.createElement("nav");
+//     newNav.textContent = "This is our new navbar";
+//     aboutSection.prepend(newNav);
+// });
