ac0c498aac2c5422613de08d8404bbaa1fdf51e1
Chris Kramer <ChrisKramer15@outlook.com>
2020-12-12 23:03:50 -0600

    list items accurately displayed using js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-// let navBar = document.querySelector("#beginBtn");
-// let aboutSection = document.querySelector("#about");
+//creating and inputing nav bar when intro button is clicked
+let navBar = document.querySelector("#beginBtn");
+let aboutSection = document.querySelector("#about");
 
-// navBar.addEventListener("click", () => {
-//     let newNav = document.createElement("nav");
-//     newNav.textContent = "This is our new navbar";
-//     aboutSection.prepend(newNav);
-// });
+navBar.addEventListener("click", () => {
+    let newNav = document.createElement("nav");
+    let newUl = document.createElement("ul");
+    let newLi1 = document.createElement("li");
+    let newLi2 = document.createElement("li");
+    aboutSection.prepend(newNav);
+    newLi1.textContent = "Home";
+    newLi2.textContent = "About";
+    newNav.prepend(newUl);
+    newUl.append(newLi1);
+    newUl.append(newLi2);
+});
