03ca7267c0de02df607309f14a7a058112bcd992
Chris Kramer <ChrisKramer15@outlook.com>
2020-12-14 18:46:11 -0600

    added javascript to not show navbar until the intro button is clicked

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// let toggle = document.querySelector('#toggle'),
+//     sidebar = document.querySelector('#sidebar');
 
+// toggle.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     sidebar.classList.toggle('hidden');
+// });
+
+let navBtn = document.querySelector("#beginBtn");
+let newNav = document.querySelector("#toggle");
+
+navBtn.addEventListener("click", () => {
+    newNav.classList.add("show");
+});
