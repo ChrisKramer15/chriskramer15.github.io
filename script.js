bde47d31f3b26a1f5023fd33cdad89f4478148de
Chris Kramer <ChrisKramer15@outlook.com>
2020-12-14 18:47:05 -0600

    missed some changes, connected to last commit

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// let toggle = document.querySelector('#toggle'),
-//     sidebar = document.querySelector('#sidebar');
-
-// toggle.addEventListener('click', function(e) {
-//     e.preventDefault();
-//     sidebar.classList.toggle('hidden');
-// });
-
 let navBtn = document.querySelector("#beginBtn");
 let newNav = document.querySelector("#toggle");
 
