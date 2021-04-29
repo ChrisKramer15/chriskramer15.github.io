d6df1ba9022993d7db801cea8e2054dd63abe9a2
Chris Kramer <ChrisKramer15@outlook.com>
2021-04-29 13:44:45 -0500

    full web version completed, responsiveness is being worked on

---
+++ script.js
@@ -0,0 +1,21 @@
+function initMap(){
+
+    // Map option
+
+    var options = {
+        center: {lat: 39.0997 , lng:-94.5786 },
+        zoom: 10
+    }
+
+    //New Map
+    map = new google.maps.Map(document.getElementById("map"),options)
+
+
+
+
+
+    
+
+
+
+}
