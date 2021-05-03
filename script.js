2ba8be6a39d38871af4097a930c5e6ba527389ea
Chris Kramer <ChrisKramer15@outlook.com>
2021-05-03 17:50:00 -0500

    mobile menu functional, resizing home image in progress

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 function initMap(){
 
     // Map option
-
     var options = {
         center: {lat: 39.0997 , lng:-94.5786 },
         zoom: 10
     }
-
     //New Map
     map = new google.maps.Map(document.getElementById("map"),options)
-
-
-
-
-
-    
-
-
-
 }
+
+//toggle navigation menu for mobile
+let toggleBtn = document.getElementsByClassName("toggle-button")[0];
+let wrapperEl = document.getElementsByClassName("wrapper")[0];
+
+toggleBtn.addEventListener('click', () => {
+    wrapperEl.classList.toggle("active");
+});
+
